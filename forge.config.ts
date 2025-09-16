28aa9ca8901f0c23e037b49938f777aa6d8ba0f3
miltonmatews <miltonmatews@gmail.com>
2025-09-16 02:29:09 -0300

    projeto adaptado para usar typescript ao invéz de javascript

---
+++ forge.config.ts
@@ -0,0 +1,40 @@
+// forge.config.ts
+import type { ForgeConfig } from '@electron-forge/shared-types';
+import { VitePlugin } from '@electron-forge/plugin-vite';
+
+const config: ForgeConfig = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    new VitePlugin({
+      build: [
+        { entry: 'src/main/main.ts', config: 'vite.main.config.mjs' },
+        { entry: 'src/preload/preload.ts', config: 'vite.preload.config.mjs' },
+      ],
+      renderer: [
+        { name: 'main_window', config: 'vite.renderer.config.mjs' },
+      ],
+    }),
+  ],
+};
+
+export default config;
